91fd9853b1e348e2c527a2c468c64eff6cc1ebad
bou1der <krowkin@bk.ru>
2024-11-14 20:55:23 +0300

    Update docker

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -10,7 +10,7 @@ export default function Home(){
     <>
       <WelcomeBlock theme="MAIN" >
         <h1 className="text-secondary-foreground text-center max-w-[832px] font-medium">
-          Раскройте тайны с «<b className="italic text-2xl sm:text-5xl font-bold">Ключом Души</b>» — уникальные расчеты для вашего пути
+          Раскройте тайны с <b className="italic text-2xl sm:text-5xl font-bold">«Ключом Души»</b> — уникальные расчеты для вашего пути
         </h1>
         <p className="text-secondary-foreground max-w-[832px] text-center">
           Откройте для себя персональную нумерологическую консультацию — ваш путь к самопознанию и гармонии начинается здесь
